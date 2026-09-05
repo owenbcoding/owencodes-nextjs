5e7eee26011e8d6c5f9295a2d3dc9016088ac30f
owenbcoding <eoghankb@gmail.com>
2026-09-05 21:43:16 +0100

    feat: add Sanity configuration file for project setup

---
+++ sanity.config.ts
@@ -0,0 +1,14 @@
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { schemaTypes } from "./src/sanity/schemaTypes";
+
+export default defineConfig({
+  name: "owencodes-portfolio",
+  title: "Owencodes Portfolio",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "missing-project-id",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
+  basePath: "/studio",
+  plugins: [structureTool(), visionTool()],
+  schema: { types: schemaTypes },
+});
